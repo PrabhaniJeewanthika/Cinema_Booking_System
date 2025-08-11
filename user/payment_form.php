b16b9ffee3dbf8e93454ec42efe81138d94ea61c
V.P.Perera <vinodyaperera20@gmail.com>
2025-08-11 21:43:45 +0530

    add payment form

--- user/payment_form.php
+++ user/payment_form.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cinema Payment System</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../assets/css/Payment-Style.css">
 </head>
 <body>
     <div class="container">
